ba4a7d68a7d227aabfee67a381d917aa5214061b
boiwantlearncode <ibetank@gmail.com>
2020-12-13 13:29:08 +0800

    Updates

--- src/ServerScriptService/main.server.lua
+++ src/ServerScriptService/main.server.lua
@@ -3,6 +3,3 @@ mazeGenerator = require(game.ServerScriptService["Maze Generation"]["Maze Genera
 
 -- Running functions
 mazeGenerator(10, 10)
-
-local unit = game.Workspace["Unit 1"]
-print(unit:GetExtentsSize())
